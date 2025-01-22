d89e4752f7fab18f0e98cdfce9ca88739418915b
LateFR <rigo.lucas@icloud.com>
2025-01-22 11:54:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,37 @@ function newColor(){
 
 setInterval(()=>{
   document.body.style.background = `
-    radial-gradient(
-      circle at 50% 0,
-      ${newColor()},
-      ${newColor()} 70.71%
+     background: repeating-linear-gradient(
+      190deg,
+      newColor() 40px,
+      newColor() 80px,
+      newColor() 120px,
+      newColor() 160px,
+      newColor() 200px,
+      newColor() 240px,
+      newColor() 280px,
+      newColor() 300px
     ),
-    radial-gradient(
-      circle at 6.7% 75%,
-      ${newColor()},
-      ${newColor()} 70.71%
+    repeating-linear-gradient(
+      -190deg,
+      newColor() 30px,
+      newColor() 60px,
+      newColor() 90px,
+      newColor() 120px,
+      newColor() 150px,
+      newColor() 180px,
+      newColor() 210px,
+      newColor() 230px
     ),
-    radial-gradient(
-      circle at 93.3% 75%,
-      ${newColor()},
-      ${newColor()} 70.71%
-      )
-      ${newColor()}`
+    repeating-linear-gradient(
+      23deg,
+      newColor() 50px,
+      newColor() 100px,
+      newColor() 150px,
+      newColor() 200px,
+      newColor() 250px,
+      newColor() 300px,
+      newColor() 350px,
+      newColor() 370px
+    )`
 },1000)
