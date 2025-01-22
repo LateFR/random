d50e36c2112ef3d2ac3d8a6e5686d046c9571f52
LateFR <rigo.lucas@icloud.com>
2025-01-22 14:08:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,39 +2,41 @@ function newColor(){
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
 }
+function random_px(){
+  return Math.floor(Math.random()*500)
 
 setInterval(()=>{
   document.querySelector("div").style.background = `repeating-linear-gradient(
           190deg,
-          ${newColor()} 40px,
-          ${newColor()} 80px,
-          ${newColor()} 120px,
-          ${newColor()} 160px,
-          ${newColor()} 200px,
-          ${newColor()} 240px,
-          ${newColor()} 280px,
-          ${newColor()} 300px
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px
         ),
         repeating-linear-gradient(
           -190deg,
-          ${newColor()} 30px,
-          ${newColor()} 60px,
-          ${newColor()} 90px,
-          ${newColor()} 120px,
-          ${newColor()} 150px,
-          ${newColor()} 180px,
-          ${newColor()} 210px,
-          ${newColor()} 230px
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px
         ),
         repeating-linear-gradient(
           23deg,
-          ${newColor()} 50px,
-          ${newColor()} 100px,
-          ${newColor()} 150px,
-          ${newColor()} 200px,
-          ${newColor()} 250px,
-          ${newColor()} 300px,
-          ${newColor()} 350px,
-          ${newColor()} 370px
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px,
+          ${newColor()} ${random_px()}px
         )`;
 },1000)
