fe5b308ca0526b5c52a0624dcfa283916399d299
LateFR <rigo.lucas@icloud.com>
2025-01-24 15:06:13 +0100

    Update history.js

--- history.js
+++ history.js
@@ -10,12 +10,18 @@ document.addEventListener("keydown",(event)=>{
         if (target>10){
             target=10
         }
+        if (target<0){
+            target=0
+        }
         document.body.style.background=history[target]
     }
     else if (event.code=="ArrowRight"){
         target-=1
         if (target<0){
             target=0
+        }
+        if (target>10){
+            target=10
         }
         document.body.style.background=history[target]
     }
